16bedfdddb22055fbdcf522d29649e3984cf4fe8
fumiya5863 <fumiya5863@gmail.com>
2024-11-24 16:00:32 +0900

    XSS Vulnerability-6

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     user_input = request.args.get("input", "")
-    safe_user_input = html.escape(user_input)
-    return render_template_string(f"<p>入力された内容: {safe_user_input}</p>")
+    return render_template_string(f"<p>入力された内容: {user_input}</p>")
 
 if __name__ == "__main__":
     app.run(debug=True)
